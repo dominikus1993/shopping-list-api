d5ca71b126d9e51dde8d353fc5cffa402bf4c40f
Dominik.Kotecki <dominikus1910@gmail.com>
2022-01-24 19:05:25 +0100

    init unicron

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from typing import Optional
+import asyncio
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/{name}")
+async def read_root(name: str):
+    return {"Hello": name}
 
 
-print("XD")
+@app.get("/{customer_id}")
+async def read_item(customer_id: int, q: Optional[str] = None):
+    result = [{"id": customer_id, "name": "John"}]
+    return result
+
+
+

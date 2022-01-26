6da83b237313f8badcc77d1cc2c8c24a18fd5691
Dominik.Kotecki <dominikus1910@gmail.com>
2022-01-26 19:23:45 +0100

    small changes

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from typing import Optional
 import asyncio
 from fastapi import FastAPI
 
+from core.usecase import CustomerShoppingListDto
+
 app = FastAPI()
+
+
 
 @app.get("/{name}")
 async def read_root(name: str):
     return {"Hello": name}
 
 
-@app.get("/{customer_id}")
+@app.get("/{customer_id}", response_model=CustomerShoppingListDto)
 async def read_item(customer_id: int):
     result = [{"id": customer_id, "name": "John"}]
     return result
